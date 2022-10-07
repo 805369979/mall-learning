217276e61adbc3104e2d80376fdc3d5cd1dbf262
qinbinbin <805369979@qq.com>
2022-10-07 18:56:11 +0800

    redis

--- springfox/src/main/java/com/mybatis/mybatis/SpringFoxApplication.java
+++ springfox/src/main/java/com/mybatis/mybatis/SpringFoxApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringFoxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RedisApplication.class, args);
+        SpringApplication.run(SpringFoxApplication.class, args);
     }
 
 }
--- springfox/src/main/java/com/mybatis/mybatis/config/Swagger2Config.java
+++ springfox/src/main/java/com/mybatis/mybatis/config/Swagger2Config.java
@@ -17,6 +17,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -50,7 +51,6 @@ public class Swagger2Config {
         return new ApiInfoBuilder()
                 .title("mall-learning")
                 .description("mall-learning")
-                .contact("mall")
                 .version("1.0")
                 .build();
     }
